40e26a19cdd435ce630bf75a1eb424621c5b70fa
rorojiao <rorojiao@gmail.com>
2026-03-06 15:32:42 +0800

    Update Monetag sw.js for tools.gamezipper.com verification

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10687716
+    "zoneId": 10689332
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
