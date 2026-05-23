bfffeeba4e6a820ebbffd3e8911f7c48f3e09b71
ops-gamezipper <ops@gamezipper.com>
2026-05-24 06:26:46 +0800

    fix: update sw.js zoneId to 11012012 (new push notification zone)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10689348
+    "zoneId": 11012012
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
